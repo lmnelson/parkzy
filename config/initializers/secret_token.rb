65c9edf964ad51586799899371b056bcc583a608
Lee Nelson <contact@leenelsondesign.com>
2013-04-07 20:26:45 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parkzy::Application.config.secret_token = '337501c35eeaec861d752fa97ca12a0896ab975eaea0b87d41137bf31cbfebfa6e908387da62466978c08313e17abd0dc36a33edb88fc713af3627c87f61ae11'
