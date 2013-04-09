8382d61a6614ec41c50be8ac426898e214d91203
Lee Nelson <contact@leenelsondesign.com>
2013-04-08 18:36:39 -0700

    updates email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
 
+  before_save { |user| user.email = user.email.downcase  }
+
   validates :name, 	presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[\w+\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
-end
+  									uniqueness: { case_sensitive: false }
+end									
