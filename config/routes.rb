1ab6bac3bb18e2135c4293de23a7d7f76f33a01c
Lee Nelson <contact@leenelsondesign.com>
2013-04-09 11:47:39 -0700

    finish sign out

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Parkzy::Application.routes.draw do
   
   match '/signup',    to: 'users#new'
   match '/signin',    to: 'sessions#new'
-  match '/signout',    to: 'sessions#destory'
+  match '/signout',    to: 'sessions#destroy', via: :delete
   
 
   # The priority is based upon order of creation:
