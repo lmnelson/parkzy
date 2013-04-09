bad6d48bdd080a9c77483a5f82b688c90bb80262
Lee Nelson <contact@leenelsondesign.com>
2013-04-08 17:52:28 -0700

    updates signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Parkzy::Application.routes.draw do
   
-  get "users/new"
-
   root :to => 'static_pages#home' 
 
   match '/help',      to: 'static_pages#help'
   match '/contact',   to: 'static_pages#contact'
   match '/about',     to: 'static_pages#about'
+  match '/signup',    to: 'users#new'
   
 
   # The priority is based upon order of creation:
